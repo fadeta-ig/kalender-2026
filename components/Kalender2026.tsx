dc8246766af56eb863d5469368bfc559c1006933
fadeta-ig <fadeta287@gmail.com>
2025-11-04 15:32:11 +0700

    commit f1e2d3c4b5a6g7h8i9j0k1l2m3n4o5p6q7r8s9t0

--- components/Kalender2026.tsx
+++ components/Kalender2026.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useMemo, useState } from "react";
+import { JSX, useCallback, useMemo, useState } from "react";
 
 type HolidayType = "libur" | "cuti-bersama";
 
